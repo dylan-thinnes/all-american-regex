b79f9fb097264df21fd64516b81d710d9e09ad37
Dylan Thinnes <dylan.thinnes@protonmail.com>
2025-07-12 21:41:16 +0100

    Add bold/italic mode

--- Main.hs
+++ Main.hs
@@ -9,24 +9,33 @@ data Trie = Prefix String [Trie] | Leaf String
 
 data RenderOptions = RenderOptions
   { mode :: RenderMode
-  , redRGB, whiteRGB, blueRGB :: (Int, Int, Int)
+  , redStyle, whiteStyle, blueStyle :: TextOptions
   , maxWidth :: Maybe Int
   }
+  deriving (Show, Eq, Ord)
+
+data TextOptions = TextOptions
+  { textStyle :: TextStyle
+  , textColor :: (Int, Int, Int)
+  }
+  deriving (Show, Eq, Ord)
+
+data TextStyle = TSBold | TSItalic | TSPlain
   deriving (Show, Eq, Ord)
 
 data RenderMode = HTML | ANSI | Plain
   deriving (Show, Eq, Ord)
 
-main = allAmericanRegex Plain
+main = allAmericanRegex HTML
 
 allAmericanRegex :: RenderMode -> IO ()
 allAmericanRegex mode = do
   states <- lines <$> readFile "state"
   let options = RenderOptions
         { mode = mode
-        , blueRGB = (60,59,110)
-        , redRGB = (178,34,52)
-        , whiteRGB = (255,255,255)
+        , blueStyle = TextOptions TSBold (60,59,110)
+        , redStyle = TextOptions TSBold (178,34,52)
+        , whiteStyle = TextOptions TSPlain (255,255,255)
         , maxWidth = Just 41
         }
   putStrLn $ renderTop options $ toTries states
@@ -57,9 +66,9 @@ renderTop :: RenderOptions -> [Trie] -> String
 renderTop opts@RenderOptions {..} tries =
   case mode of
     HTML ->
-      let grey = (120, 120, 120)
-          header = renderRGB HTML grey "# All-American Regex"
-          slash = renderRGB HTML grey "/"
+      let grey = TextOptions TSPlain (120, 120, 120)
+          header = renderStyle HTML grey "# All-American Regex"
+          slash = renderStyle HTML grey "/"
       in
       unlines
         [ "<style>body { background-color: black; } #regex { font-family: monospace; }</style>\n"
@@ -86,9 +95,9 @@ chunks size cs =
 
 renderOne :: RenderOptions -> Trie -> [(String, Char, String)]
 renderOne opts@RenderOptions {..} (Leaf str) =
-  renderRGB mode whiteRGB str
+  renderStyle mode whiteStyle str
 renderOne opts@RenderOptions {..} (Prefix prefix tries) =
-  renderRGB mode whiteRGB prefix ++ renderMany opts tries
+  renderStyle mode whiteStyle prefix ++ renderMany opts tries
 
 joinBookends :: [(String, Char, String)] -> String
 joinBookends = concatMap $ \(pre, c, post) -> pre ++ [c] ++ post
@@ -96,32 +105,40 @@ joinBookends = concatMap $ \(pre, c, post) -> pre ++ [c] ++ post
 renderMany :: RenderOptions -> [Trie] -> [(String, Char, String)]
 renderMany opts@RenderOptions {..} tries =
   concat
-    [ renderRGB mode blueRGB "("
-    , intercalate (renderRGB mode redRGB "|") $
+    [ renderStyle mode blueStyle "("
+    , intercalate (renderStyle mode redStyle "|") $
         map (renderOne opts) tries
-    , renderRGB mode blueRGB ")"
+    , renderStyle mode blueStyle ")"
     ]
 
-renderRGB :: RenderMode -> (Int, Int, Int) -> String -> [(String, Char, String)]
-renderRGB mode color str =
+renderStyle :: RenderMode -> TextOptions -> String -> [(String, Char, String)]
+renderStyle mode color str =
   let (pre, post) = rgbBookends mode color
   in
   map (pre,,post) str
 
-rgbBookends :: RenderMode -> (Int, Int, Int) -> (String, String)
-rgbBookends ANSI (r, g, b) =
+rgbBookends :: RenderMode -> TextOptions -> (String, String)
+rgbBookends ANSI (TextOptions style (r, g, b)) =
   ( concat
       [ "\ESC[38;2;"
       , intercalate ";" $ map show [r, g, b]
       , "m"
-      ]
+      ] ++ case style of
+             TSBold -> "\ESC[1m"
+             TSItalic -> "\ESC[3m"
+             _ -> ""
   , "\ESC[0m"
   )
-rgbBookends HTML (r, g, b) =
+rgbBookends HTML (TextOptions style (r, g, b)) =
   ( concat
       [ "<span style=\"color: rgb("
       , intercalate "," $ map show [r, g, b]
-      , ")\">"
+      , ");"
+      , case style of
+          TSBold -> " font-weight: bold;"
+          TSItalic -> " font-style: italic;"
+          _ -> ""
+      , "\">"
       ]
   , "</span>"
   )
